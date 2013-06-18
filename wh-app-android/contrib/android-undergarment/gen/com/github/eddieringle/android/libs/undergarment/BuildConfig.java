41e76dfa806fb909947eb20b730e92807f877c9e
xzg <x@u.(none)>
2013-06-18 22:56:27 +0800

    new repository

--- wh-app-android/contrib/android-undergarment/gen/com/github/eddieringle/android/libs/undergarment/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.github.eddieringle.android.libs.undergarment;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
